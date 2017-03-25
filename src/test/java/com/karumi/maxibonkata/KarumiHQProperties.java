a5822cfdc1b138bcfe4b0f2fb4b26c07178500ed
Iván Garza <ivngrz@gmail.com>
2017-03-25 12:57:54 +0100

    Test generating number of items

--- src/test/java/com/karumi/maxibonkata/KarumiHQProperties.java
+++ src/test/java/com/karumi/maxibonkata/KarumiHQProperties.java
@@ -3,6 +3,7 @@ package com.karumi.maxibonkata;
 import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import java.util.List;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 
@@ -22,6 +23,14 @@ public class KarumiHQProperties {
   public void numberOfMaxibonsIsAlwaysOverTwo(
       @From(DevelopersGenerator.class) Developer developer) {
     karumiHQs.openFridge(developer);
+
+    assertTrue(karumiHQs.getMaxibonsLeft() > 2);
+  }
+
+  @Property
+  public void afterAGroupOfKarumiesOpenTheFridgeTheNumberOfMaxibonsIsOverTwo(
+      List<@From(KarumiesGenerator.class) Developer> developers) {
+    karumiHQs.openFridge(developers);
 
     assertTrue(karumiHQs.getMaxibonsLeft() > 2);
   }
